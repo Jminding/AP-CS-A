25b63efdd0667465444a1bc9f0ba64c370c2aa08
Jaymin Ding <jtding43@gmail.com>
2022-01-26 14:50:25 -0500

    Remove Problem

--- ArrayInsert.java
+++ ArrayInsert.java
@@ -1,12 +1,12 @@
-import java.util.*;
-
 public class ArrayInsert {
     public static String[] arr = {"A", "B", "C", "D", "E", "F", "G", null, null};
+
     public static void main(String[] args) {
         insert("Sus", 5);
         for (String s : arr) System.out.print(s + " ");
         System.out.println();
     }
+
     public static void insert(String word, int pos) {
         if (arr.length - 1 - pos >= 0) System.arraycopy(arr, pos, arr, pos + 1, arr.length - 1 - pos);
         arr[pos] = word;
--- ArrayRemove.java
+++ ArrayRemove.java
@@ -1,10 +1,14 @@
-import java.util.*;
-
 public class ArrayRemove {
+    public static String[] a = {"A", "B", "C", "D", "E", "F", "G"};
+
     public static void main(String[] args) {
-
+        remove(3);
+        for (String s : a) System.out.print(s + " ");
+        System.out.println();
     }
-    public void remove(int[] a, int pos) {
 
+    public static void remove(int pos) {
+        if (a.length - 1 - pos >= 0) System.arraycopy(a, pos + 1, a, pos, a.length - 1 - pos);
+        a[a.length - 1] = null;
     }
 }
